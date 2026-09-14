911a5ffe6d45236ca9c42cb82028ac2354732f16
WaleedHaji <waleed4haji@gmail.com>
2026-09-14 14:46:12 +0300

    feat: completed mongoose relationships lab

--- server.js
+++ server.js
@@ -20,8 +20,6 @@ dns.setServers(["8.8.8.8", "1.1.1.1"])
 
 
 
-
-
 // Middleware
 app.use(express.static('public')); //all static files are in the public folder
 app.use(express.urlencoded({ extended: false })); // this will allow us to see the data being sent in the POST or PUT
@@ -49,24 +47,42 @@ conntectToDB()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 // Routes go here
+async function testRelationships(){
+    const createUser = await User.create({
+        username: 'BuDawud',
+        password: 'a1b2c3d4'
+    })
+    console.log(createUser)
+}
+// // testRelationships()
 
 
+async function testRelationships(){
+    const foundUser = await User.find()
+    console.log(foundUser)
+}
+// testRelationships()
+
+async function testRelationships(){
+    const createListing = await Listing.create({
+        streetAddress: 'building 13 road 555' ,
+            city: 'Downtown' ,
+            price: 500 ,
+            size: 240 ,
+            owner: '6aa7c92306967135a0c37592',
+            category: 'N/A'
+    })
+    console.log(createListing)
+}
+// testRelationships()
 
 
-
+async function testRelationships(){
+    const foundListing = await Listing.find().populate('owner')
+    console.log(foundListing)
+}
+testRelationships()
 
 
 
